1ef327dcb620135f0571d11cc853747d5845e8b2
ANIRUDDHA MALLADI <malladi.aniruddha.1999@gmail.com>
2025-06-10 16:10:16 +0530

    Daily push 10-6-2025

--- Functions.R
+++ Functions.R
@@ -47,3 +47,68 @@ v4 = function(len, dia, rad) {
   return (vol)
 }
 print(v4(dia = 5, len = 10))
+
+
+# Multiple output is not supported by R , but we can do so by storing them in a list and returning the list
+v5 = function(dia, len) {
+  vol = pi*dia^2*len/4
+  surface_area = pi*dia*len
+  result = list("volume"=vol, "surface_area"=surface_area)
+  return(result)
+}
+
+print(v5(10, 5))
+
+# In line function
+func = function(x) x^2 + 4*x + 4
+print(func(2))
+
+# Looping over objects
+# 1. apply: Apply the function over margins of an array or matrix
+# 2. lapply: apply a function over a list or a vector
+# 3. tapply: apply a function over ragged array
+# 4. mapply: multivariant vesion of lappy
+# 5. xxply: plye package
+
+# apply
+# x: determines the input array including matrix.
+# margin: If the margin is 1 function is applied across row, 
+# if the margin is 2 it is applied across the column.
+# function: determines the function that is to be applied on input data.
+
+sample_matrix <- matrix(C<-(1:10),nrow=3, ncol=10)
+
+print(apply(sample_matrix, 2, sum))
+
+print(apply(sample_matrix, 1, mean))
+
+# lapply
+# x: determines the input vector or an object.
+# fun: determines the function that is to be applied to input data.
+
+A1 = matrix(1:9,3,3)
+B1 = matrix(10:18,3,3)
+mylist = list(A1, B1)
+det_result = lapply(mylist, det)
+print(det_result)
+
+# mapply
+# apply functions across lists
+
+v6 = function(dia, len) {
+  result = pi*(dia/2)^2*len
+  return(result)
+}
+
+dia = c(1:4)
+len = c(7, 4, 3, 2)
+
+volume = mapply(v6, dia, len)
+print(volume)
+
+# tapply
+# tapply is used to apply a function over subset of vectors given by a combination of factors
+
+Id = c(1,1,1,1,2,2,2,3,3)
+Values = (1:9)
+tapply(Values,Id, sum)
